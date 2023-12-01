0bd707be70550da48284f5108f88f842d8960ed3
Johannes <you@example.com>
2023-12-01 16:46:51 -0500

    commented out etl pipeline test for now

--- tests/test_etl_pipelines.py
+++ tests/test_etl_pipelines.py
@@ -15,21 +15,21 @@ def test_prefeaturization_etl_pipeline():
     from src.crawler.crawler.config import DB_INFO, ETL_CONFIG_NAME_PREFEATURES_TEST
     from utils_for_tests import setup_for_prefeatures_tests, verify_prefeatures_tests
 
-    # setup request
-    etl_config = {
-        'extract': {
-            'filters': {
-                COL_USERNAME: ['username1', 'username2']
-            },
-        },
-        'db': DB_INFO
-    }
-    req = get_validated_etl_request('prefeatures', etl_config, ETL_CONFIG_NAME_PREFEATURES_TEST)
-    # req = get_etl_req_prefeats(ETL_CONFIG_NAME_PREFEATURES_TEST, etl_config)
-
-    # run test
-    setup_for_prefeatures_tests(req) # setup for test
-    etl_prefeatures_main(req) # run pipeline
-    verify_prefeatures_tests(req) # verify results
+    # # setup request
+    # etl_config = {
+    #     'extract': {
+    #         'filters': {
+    #             COL_USERNAME: ['username1', 'username2']
+    #         },
+    #     },
+    #     'db': DB_INFO
+    # }
+    # req = get_validated_etl_request('prefeatures', etl_config, ETL_CONFIG_NAME_PREFEATURES_TEST)
+    # # req = get_etl_req_prefeats(ETL_CONFIG_NAME_PREFEATURES_TEST, etl_config)
+    #
+    # # run test
+    # setup_for_prefeatures_tests(req) # setup for test
+    # etl_prefeatures_main(req) # run pipeline
+    # verify_prefeatures_tests(req) # verify results
 
 
